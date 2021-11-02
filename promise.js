a272063b5f6540b1f76c9d4b5fe24ae899a5dd35
vhorozheiev <vhorozheiev@gmail.com>
2021-11-02 14:50:18 +0200

    test variant of solution

--- promise.js
+++ promise.js
@@ -9,38 +9,80 @@ first function is done and print the final message like `Your number is ${result
 when the first function doesn't return " error“
 5. Write same tasks(1-4) using async/await */
 
-function checkNumber(data, res, rej) {
-    if (typeof data !== 'number') return rej(Error('error'));
-    res(data);
+// function checkNumber(data, res, rej) {
+//     if (typeof data !== 'number') return rej(Error('error'));
+//     res(data);
+// }
+
+// function isOddNumber(number, res, rej) {
+//     if (number % 2 == 0) {
+//         setTimeout(() => {
+//             console.log("even " + number);
+//             return rej(number);
+//         }, 2000);
+//     }
+//     else {
+//         setTimeout(() => {
+//             console.log("odd");
+//             res(number);
+//         }, 1000);
+//     }
+// }
+
+// function printResult(result) {
+//     console.log(`Your number is ${result}`);
+// }
+
+// new Promise((res, rej) => {
+//     checkNumber(14, res, rej)
+// }).then(val => {
+//     new Promise((res, rej) => {
+//         isOddNumber(val, res, rej);
+//     }).then(val => {
+//         printResult(val);
+//     }).catch(val => {
+//         printResult(val);
+//     })
+// });
+
+//async realize
+
+async function checkNumber(data) {
+    return new Promise((res, rej) => {
+        if (typeof data !== 'number') return rej(Error('error'));
+        res(data);
+    });
 }
 
-function isOddNumber(number, res, rej) {
-    if (number % 2 == 0) {
-        setTimeout(() => {
-            console.log("even " + number);
-            return rej(number);
-        }, 2000);
-    }
-    else {
-        setTimeout(() => {
-            console.log("odd");
-            res(number);
-        }, 1000);
-    }
-}
-
-function printResult(result) {
-    console.log(`Your number is ${result}`);
-}
-
-new Promise((res, rej) => {
-    checkNumber(14, res, rej)
-}).then(val => {
-    new Promise((res, rej) => {
-        isOddNumber(val, res, rej);
-    }).then(val => {
-        printResult(val);
-    }).catch(val => {
-        printResult(val);
+async function isOddNumber(number) {
+    return new Promise((res, rej) => {
+        if (number % 2 == 0) {
+            setTimeout(() => {
+                console.log("even ");
+                return rej(number);
+            }, 2000);
+        }
+        else {
+            setTimeout(() => {
+                console.log("odd");
+                res(number);
+            }, 1000);
+        }
     })
-});
+}
+
+//need to fix
+async function printResult(result) {
+    return new Promise((res, rej) => {
+        console.log(`Your number is ${result}`);
+    }).catch(result => {
+        console.log(`Your number is ${result}`);
+    });
+}
+async function promiseRunner() {
+    let checkNumberResult = await checkNumber(10);
+    let checkIsOddNumber = await isOddNumber(checkNumberResult);
+    let printRes = await printResult(checkIsOddNumber);
+}
+
+promiseRunner();
